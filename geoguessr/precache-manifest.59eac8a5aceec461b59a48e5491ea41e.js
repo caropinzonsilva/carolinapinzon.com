d2c888da171b72931999f22420b8dd820d1d18c1
Carolina Pinzon <caropinzon2010@gmail.com>
2020-01-22 22:11:51 +0000

    Upload New File

---
+++ geoguessr/precache-manifest.59eac8a5aceec461b59a48e5491ea41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9aa796b25189c69c6a7b5cc4ef6ed38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9de7ce4dbf33f565d974",
+    "url": "/static/css/main.9e96fce1.chunk.css"
+  },
+  {
+    "revision": "e9ad17c7c265ece9bc8e",
+    "url": "/static/js/2.0d1f5267.chunk.js"
+  },
+  {
+    "revision": "eb475303f44e7b98ae66d69268b7dde2",
+    "url": "/static/js/2.0d1f5267.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9de7ce4dbf33f565d974",
+    "url": "/static/js/main.1b245392.chunk.js"
+  },
+  {
+    "revision": "8684f3f3fb57e3d909f0",
+    "url": "/static/js/runtime-main.b706b430.js"
+  }
+]);
